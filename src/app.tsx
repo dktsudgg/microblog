cd4fec3e170d3bc843c1d0774e537f5405b68bc6
dktsudgg <dktsudgg@gmail.com>
2026-07-08 00:38:16 +0900

    feat: setup page

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,18 @@
 import { Hono } from "hono";
 import { federation } from "@fedify/hono";
 import fedi from "./federation.ts";
+import { Layout, SetupForm } from "./views.tsx";
 
 const app = new Hono();
 app.use(federation(fedi, () => undefined));
 
 app.get("/", (c) => c.text("Hello, Fedify!"));
+app.get("/setup", (c) => 
+  c.html(
+    <Layout>
+      <SetupForm />
+    </Layout>
+  ),
+);
 
 export default app;
