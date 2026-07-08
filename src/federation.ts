fc98846043f8b607c54af476cc87a72b6c1cea9f
dktsudgg <dktsudgg@gmail.com>
2026-07-08 23:45:49 +0900

    feat: actor가 activity 송/수신 시 필요한 전자서명용 키 dispatcher 구현

--- src/federation.ts
+++ src/federation.ts
@@ -1,9 +1,14 @@
-import { createFederation } from "@fedify/fedify";
+import {
+  createFederation,
+  exportJwk,
+  generateCryptoKeyPair,
+  importJwk,
+} from "@fedify/fedify";
 import { Person, Endpoints } from "@fedify/vocab";
 import { getLogger } from "@logtape/logtape";
 import { InProcessMessageQueue, MemoryKvStore } from "@fedify/fedify";
 import db from "./db.ts";
-import type { Actor, User } from "./schema.ts";
+import type { Actor, User, Key, } from "./schema.ts";
 
 const logger = getLogger("microblog");
 
@@ -23,7 +28,8 @@ federation.setActorDispatcher("/users/{identifier}", async (ctx, identifier) => 
   if (user == null) return null;
 
   logger.info("Dispatching actor {identifier}", { identifier });
-  
+
+  const keys = await ctx.getActorKeyPairs(identifier);
   return new Person({
     id: ctx.getActorUri(identifier),
     preferredUsername: identifier,
@@ -33,7 +39,54 @@ federation.setActorDispatcher("/users/{identifier}", async (ctx, identifier) => 
       sharedInbox: ctx.getInboxUri(),
     }),
     url: ctx.getActorUri(identifier),
+    publicKey: keys[0].cryptographicKey,
+    assertionMethods: keys.map((k) => k.multikey),
   });
+})
+.setKeyPairsDispatcher(async (ctx, identifier) => {
+  const user = db.prepare<unknown[], User>("select * from users where username = ?").get(identifier);
+  if (user == null) return [];
+
+  const rows = db.prepare<unknown[], Key>("select * from keys where keys.user_id = ?").all(user.id);
+  const keys = Object.fromEntries(rows.map((row) => [row.type, row])) as Record<Key["type"], Key>;
+  
+  const pairs: CryptoKeyPair[] = [];
+  // 사용자가 지원하는 두 키 형식 (RSASSA-PKCS1-v1_5 및 Ed25519) 각각에 대해
+  // 키 쌍을 보유하고 있는지 확인하고, 없으면 생성 후 데이터베이스에 저장:
+  for (const keyType of ["RSASSA-PKCS1-v1_5", "Ed25519"] as const) {
+    if (keys[keyType] == null) {
+      logger.debug(
+        "The user {identifier} does not have an {keyType} key; creating one...",
+        { identifier, keyType },
+      );
+
+      const { privateKey, publicKey } = await generateCryptoKeyPair(keyType);
+      db.prepare(
+        `
+        INSERT INTO keys (user_id, type, private_key, public_key)
+        VALUES (?, ?, ?, ?)
+        `,
+      ).run(
+        user.id,
+        keyType,
+        JSON.stringify(await exportJwk(privateKey)),
+        JSON.stringify(await exportJwk(publicKey)),
+      );
+      pairs.push({ privateKey, publicKey });
+    } else {
+      pairs.push({
+        privateKey: await importJwk(
+          JSON.parse(keys[keyType].private_key),
+          "private",
+        ),
+        publicKey: await importJwk(
+          JSON.parse(keys[keyType].public_key),
+          "public",
+        ),
+      });
+    }
+  }
+  return pairs;
 });
 
 federation.setInboxListeners("/users/{identifier}/inbox", "/inbox");
--- src/schema.ts
+++ src/schema.ts
@@ -16,3 +16,11 @@ export interface Actor {
   created: string;
 }
 
+export interface Key {
+  user_id: number;
+  type: "RSASSA-PKCS1-v1_5" | "Ed25519";
+  private_key: string;
+  public_key: string;
+  created: string;
+}
+
